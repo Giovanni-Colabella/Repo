6eb2ff1504272241ea8d35c761002c0bd7f3749c
Giovanni Colabella <giovannicolabell@gmail.com>
2024-05-10 16:45:23 +0200

    java lambda

--- IntelliJ/Project1/src/Main.java
+++ IntelliJ/Project1/src/Main.java
@@ -1,21 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@FunctionalInterface
+interface A {
+    int add (int i, int j);
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        Alien alien = new Alien("John", "Surname", 24);
-
-        System.out.println("Code changed from github editor");
-
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println("test");
-            System.out.println("ciao");
-        }
+        System.out.println("Hello world");
+        A obj = (i,j) -> i+j;
+        int result = obj.add(2, 5);
+        System.out.println("result is: " +result);
     }
 }
