d00ea0dc1b01027f8fe788d700517d46cb092a8a
Giovanni Colabella <giovannicolabell@gmail.com>
2024-05-10 18:07:02 +0200

    releasing SET Java

--- IntelliJ/Project1/src/Main.java
+++ IntelliJ/Project1/src/Main.java
@@ -1,12 +1,46 @@
-@FunctionalInterface
-interface A {
-    int add (int i, int j);
+import java.util.*;
+
+class NavinException extends Exception {
+    public NavinException(String string) {
+        super(string);
+    }
 }
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        A obj = (i,j) -> i+j;
-        int result = obj.add(2, 5);
-        System.out.println("result is: " +result);
+        /*int i = 20;
+        int j = 0;
+
+        int nums[] = new int[5];
+
+        try {
+            System.out.println(nums[1]);
+            System.out.println(nums[4]);
+            j = 18/i;
+            if(j == 0){
+                throw new NavinException("i dont want to print zero");
+            }
+        }
+        catch (NavinException e){
+            j = 18;
+            System.out.println("that's the default output " +e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.print("Array index out of bounds... stay in your limits!");
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong!");
+        }
+
+        System.out.println(j);*/
+        Set<Integer> nums = new HashSet<Integer>();
+        nums.add(3);
+        nums.add(7);
+        nums.add(2);
+        nums.add(8);
+
+        for(int n : nums) {
+            System.out.println(n);
+        }
     }
 }
