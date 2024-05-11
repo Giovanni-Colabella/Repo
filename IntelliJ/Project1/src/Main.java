f056f7b5b149515eba1c99b5b47827c96c56275d
Giovanni Colabella <giovannicolabell@gmail.com>
2024-05-11 11:55:06 +0200

    Updated Main.java class
    Released on 2024/05/11 at 11:55

--- IntelliJ/Project1/src/Main.java
+++ IntelliJ/Project1/src/Main.java
@@ -3,15 +3,40 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+
 class NavinException extends Exception {
     public NavinException(String string) {
         super(string);
     }
 }
+class GiovanniException extends Exception {
+    public GiovanniException(String string) { System.out.println(string); }
+}
+
+class A{
+    public A(){
+        super();
+        System.out.println("in A");
+    }
+    public A(int i){
+        super();
+        System.out.println("in A int");
+    }
+}
+class B extends A{
+    public B(){
+        super();
+        System.out.println("in B");
+    }
+    public B(int i){
+        super(i);
+        System.out.println("in B int");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        /*int i = 20;
+        int i = 20;
         int j = 0;
 
         int nums[] = new int[5];
@@ -19,14 +44,16 @@ public class Main {
         try {
             System.out.println(nums[1]);
             System.out.println(nums[4]);
-            j = 18/i;
             if(j == 0){
-                throw new NavinException("i dont want to print zero");
+                throw new GiovanniException("Attenzione, riscontrato errore... riprovare piu' tardi");
             }
         }
-        catch (NavinException e){
+        /*catch (NavinException e){
             j = 18;
             System.out.println("that's the default output " +e);
+        }*/
+        catch(GiovanniException e){
+            System.out.println("GiovanniException ");
         }
         catch (ArrayIndexOutOfBoundsException e){
             System.out.print("Array index out of bounds... stay in your limits!");
@@ -35,8 +62,8 @@ public class Main {
             System.out.println("Something went wrong!");
         }
 
-        System.out.println(j);*/
-        Set<Integer> nums = new HashSet<Integer>();
+        System.out.println(j);
+        /*Set<Integer> nums = new HashSet<Integer>();
         nums.add(3);
         nums.add(7);
         nums.add(2);
@@ -54,7 +81,10 @@ public class Main {
 
         for(String key : students.keySet()){
             System.out.println(key + " : " + students.get(key));
-        }
+        }*/
 
+        System.out.println();
+        System.out.println();
+        B b = new B(5);
     }
 }
